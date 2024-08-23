042047221828b2890b8803ca3bbe9a82769812da
hallanic-isep <herve.allanic@isep.fr>
2024-08-23 18:04:58 +0200

    Ajout du "for"

--- juste_prix.py
+++ juste_prix.py
@@ -1,17 +1,28 @@
 import random
 cible = random.randint(1,10)
 # Triche...
-print(f"La cible est {cible}")
-fmqsklfsklf
-essai = int(input("Votre proposition entre 1 et 10 : "))
+#print(f"La cible est {cible}")
 
-# Debug : Affiche le type des données
-print(f"\"cible\" est de type {type(cible)}")
-print(f'"essai" est de type {type(essai)}')
+for i in range(5):
 
-if cible == essai:
-    print("Bravo !!!")
-elif cible < essai:
-    print("Trop élevé...")
-else:
-    print("Trop peu...")
+    essai = int(input("Votre proposition entre 1 et 10 : "))
+
+    # Debug : Affiche le type des données
+    #print(f"\"cible\" est de type {type(cible)}")
+    #print(f'"essai" est de type {type(essai)}')
+
+    if cible == essai:
+        print("Bravo !!!")
+
+        # Force la fin de la boucle
+        break
+
+    elif cible < essai:
+        print("Trop élevé...")
+    else:
+        print("Trop peu...")
+
+if essai != cible:
+    print("Perdu...")
+
+print("FIN...")
