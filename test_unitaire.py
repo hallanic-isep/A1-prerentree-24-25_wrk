f30e522d1038cbf548ebe6f19cebfdf49e789cc0
hallanic-isep <herve.allanic@isep.fr>
2024-08-23 18:57:56 +0200

    Ajout fonction et test unitaire

---
+++ test_unitaire.py
@@ -0,0 +1,11 @@
+import unittest
+
+import juste_prix
+
+class Tests(unittest.TestCase):
+
+    def test_verifie_OK(self):
+        self.assertEqual(juste_prix.verifie(5,5), True)
+
+    def test_verifie_PB(self):
+        self.assertEqual(juste_prix.verifie(5,3), True)
